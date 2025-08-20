c423d2007815a1485f6c8c201b63ad22cb17961e
nimra ghouri <nimraghouri98@gmail.com>
2025-08-20 18:26:29 +0500

    replaced old project with new react app (ignored node_modules)

--- mongo-db-uthentation-02/myapp/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-dark text-light text-center p-3 mt-5">
-      <p>© 2025 MyApp. All rights reserved.</p>
-    </footer>
-  );
-}
